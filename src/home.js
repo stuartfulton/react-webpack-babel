7e23c079e2356cc02d8189b7c0c32ab4826c440b
unknown <sfulton@sfulton-L8.jenkon.com>
2016-01-25 14:59:05 -0600

    Save point

--- src/home.js
+++ src/home.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-console.log('home.js loaded');
-export default {};
+
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>Hello world! (react)</div>);
+  }
+}
+
+ReactDOM.render(<Example />, document.getElementById('react-container'));
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -14,5 +14,5 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-map'
+  //devtool: 'source-map'
 };
