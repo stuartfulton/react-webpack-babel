83b98d93a12479e0bf1d444078bbbf8461bd5dad
stuart <w.stuart.fulton@gmail.com>
2016-01-25 22:51:58 -0600

    Stubbed input box and handler

--- src/home.js
+++ src/home.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyInputField from './components/MyInputField';
+import FrequencyDistribution from './components/FrequencyDistribution';
 
-class Example extends React.Component {
+class Container extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      text: "",
+      characterCounts: {}
+    };
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+  }
+
+  handleKeyUp(e) {
+    var newValue = e.target.value;
+    console.log(getCharacterCounts(newValue));
+    // this.setState({
+    //   text: newValue,
+    //   characterCounts: ""//getCharacterCounts(newValue)
+    // });
   }
 
   render() {
-    return (<div>Hello world! (react)</div>);
+    return (
+      <div>
+        <MyInputField onKeyUp={this.handleKeyUp} />
+        <div className="frequency-distribution-wrapper">
+          <FrequencyDistribution characterCounts={this.state.characterCounts} />
+        <div>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<Example />, document.getElementById('react-container'));
+ReactDOM.render(<Container />, document.getElementById('react-container'));
+
+// function getCharacterCounts(text) {
+//   var characterCounts = {};
+//   for (var i in text) {
+//     var character = (text[i] + "").toLowerCase();
+//     if (!characterCounts[character]) {
+//       characterCounts[character] = 1;
+//     } else {
+//       characterCounts[character] += 1;
+//     }
+//   }
+//   return characterCounts;
+// }
