77d928892b1a753971f89343e3854cdc0e6ee48f
stuart <w.stuart.fulton@gmail.com>
2016-01-25 22:52:40 -0600

    First React components

---
+++ src/components/FrequencyDistribution.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class FrequencyDistribution extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello Character</div>
+    );
+  }
+}
---
+++ src/components/MyInputField.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class MyInputField extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <input className="text-field-large" type="text" value={this.props.value} onKeyUp={this.props.onKeyUp} />
+    );
+  }
+}
